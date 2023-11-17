d0ca06d61b9a914cda48d6316e046106953cd7b8
Lishanth <120487924+Lishanth007@users.noreply.github.com>
2023-11-17 13:31:15 +0530

    new

--- src/main/java/Mapping/pkg/Companys.java
+++ src/main/java/Mapping/pkg/Companys.java
@@ -1,12 +1,9 @@
 // Company.java
 package Mapping.pkg;
 
-import java.util.List;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
